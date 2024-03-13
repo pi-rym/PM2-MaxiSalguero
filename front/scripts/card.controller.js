414f57796a5d20ada7a40360c5f1cba04dc6d66a
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-03-13 03:51:20 -0300

    Añadiendo Async/Await

--- front/scripts/card.controller.js
+++ front/scripts/card.controller.js
@@ -1,3 +1,5 @@
+const axios = require("axios")
+
 const createCard = (img, title, director) => {
     const container = 
     `<div class="card" style="width: 18rem;">
@@ -11,14 +13,26 @@ const createCard = (img, title, director) => {
      return container
 }
 
-const renderCards = (callback) => {
-    $.get("https://students-api.2.us-1.fl0.io/movies", (datos) => {
-        const cards = datos.map((movie) => createCard(movie.poster, movie.title, movie.director));
-        callback(cards);
-    })
+const getData = async () => {
+    try {
+      const response = await axios.get("https://students-api.up.railway.app/movies"); 
+      return response.data
+    } catch (error) {
+      console.log(error.response.data);
+    }
+}
+
+const renderCards = async (callback) => {
+    try {
+      const data = await getData()
+      const cards = data.map((movie) => createCard(movie.poster, movie.title, movie.director));
+      callback(cards);
+    } catch (error) {
+      console.log(error.response.data);
+    }
 }
 
 module.exports = {
     renderCards,
     createCard,
-}
+} 

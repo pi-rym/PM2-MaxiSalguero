b9e76ed63fa0a8ade8086ccdc6ce1e681470897f
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-03-14 11:54:34 -0300

    Extra credit - Homework

--- back/src/services/moviesService.js
+++ back/src/services/moviesService.js
@@ -1,10 +1,25 @@
 const axios = require("axios")
 
+class Movie {
+    constructor(title, poster ,director){
+        if ([title, poster, director].some(prop => !prop)) throw new Error("Faltan datos");
+      
+        this.title = title,
+        this.poster = poster,
+        this.director = director
+    }
+}
+
 //Crear una funcion con async, que retorne un array con las peliculas, haciendo peticion a la api
 const getMoviesService = async () => {
     try {
       const response = await axios.get("https://students-api.up.railway.app/movies"); 
-      return response.data; 
+      const movies = [] 
+      response.data.forEach((movie) => { 
+        movies.push(new Movie(movie.title, movie.poster, movie.director));
+      })
+      return movies
+      //return response.data; 
     } catch (error) {
       console.log(error.message);
     }
